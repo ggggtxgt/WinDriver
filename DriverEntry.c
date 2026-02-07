add3391b6bdd0c0820d026ff408ae635ae18402b
ggggtxgt <3035886959@qq.com>
2026-02-07 11:56:50 +0800

    034线程相关操作

--- DriverEntry.c
+++ DriverEntry.c
@@ -1,23 +1,40 @@
 #include <ntifs.h>
 
+KEVENT kEvent;
+PETHREAD pThread;
+
+void MyThread(PVOID arg) {
+	pThread = PsGetCurrentThread();
+	for (SIZE_T i = 0; i < 10000; i++) {
+		LARGE_INTEGER timeout = RtlConvertLongToLargeInteger(-10 * 1000 * 2000);
+		NTSTATUS status = KeWaitForSingleObject(&kEvent, Executive, KernelMode, FALSE, &timeout);
+		if (STATUS_TIMEOUT == status) {
+			DbgPrint("等待超时，继续执行!!!");
+			continue;
+		} else {
+			DbgPrint("已接收信号，线程执行终止操作!!!");
+			PsTerminateSystemThread(STATUS_SUCCESS);
+		}
+	}
+}
+
 void DriverUnload(PDRIVER_OBJECT pDriver) {
+	KeSetEvent(&kEvent, 0, FALSE);
+	NTSTATUS status = KeWaitForSingleObject(pThread, Executive, KernelMode, FALSE, NULL);
+	if (NT_SUCCESS(status)) {
+		DbgPrint("线程已经终止，执行DriverUnload!!!");
+	}
 	DbgPrint("DriverUnload!!!");
 }
 
-void Func() {
-	PEPROCESS pEprocess = NULL;
-	KAPC_STATE apcStatus = { 0 };
-	NTSTATUS status = PsLookupProcessByProcessId((HANDLE)2064, &pEprocess);
-	if (NT_SUCCESS(status)) {
-		PULONG pvalue = (PULONG)0x1b60000;
-		KeStackAttachProcess(pEprocess, &apcStatus);
-		DbgPrint("value: %d", pvalue);
-	}
-	KeUnstackDetachProcess(&apcStatus);
-}
-
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING pRegPath) {
-	Func();
+	KeInitializeEvent(&kEvent, SynchronizationEvent, FALSE);
+	HANDLE hThread = 0;
+	NTSTATUS status = PsCreateSystemThread(&hThread, GENERIC_ALL, NULL, NULL, NULL, MyThread, NULL);
+	if (NT_SUCCESS(status)) {
+		DbgPrint("线程创建成功!!!");
+	}
+	NtClose(hThread);
 	pDriver->DriverUnload = DriverUnload;
 	return STATUS_SUCCESS;
 }
